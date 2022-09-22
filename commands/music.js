522aa1bafe4a83b786c0b890b75d87aca4d7edd5
Luke Gulaman <lukegulaman@gmail.com>
2022-09-22 21:15:44 +0800

    file

--- commands/music.js
+++ commands/music.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
-const { createAudioPlayer, NoSubscriberBehavior, JoinVoiceChannel } = require('@discordjs/voice');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { createAudioPlayer, NoSubscriberBehavior, JoinVoiceChannel, AudioPlayerStatus  } = require('@discordjs/voice');
 
 const player = createAudioPlayer({
   behavior: {
@@ -7,13 +7,15 @@ const player = createAudioPlayer({
   },
 });
 
+let musicQueue = []
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('music')
     .setDescription('incoming music transmission...')
     .addSubcommand(subcommand =>
-      subcommand.setName('add')
-        .setDescription('add music to the queue')
+      subcommand.setName('play')
+        .setDescription('play music')
         .addStringOption(option =>
           option.setName('link')
             .setDescription('the music link')))
@@ -22,21 +24,69 @@ module.exports = {
         .setDescription('skip the current music'))
     .addSubcommand(subcommand =>
       subcommand.setName('stop')
-        .setDescription('stop the player')),
+        .setDescription('stop the player'))
+    .addSubcommand(subcommand =>
+      subcommand.setName('play')
+      .setDescription('play music'))
+    .addSubcommand(subcommand =>
+      subcommand.setName('clear')
+      .setDescription('clear music queue')),    
 
   async execute(interaction) {
-    if (interaction.options.getSubcommand() === "add") {
-      JoinVoiceChannel({
-        channelID: interaction.member.voice.channelId,
-        guildID: interaction.guild.id,
-        adaptorCreator: interaction.guild.voiceAdaptorCreator,
+    if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
+      return void interaction.reply({ content: "what voice channel did you join??", ephemeral: true });
+  }
+
+  if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) {
+      return void interaction.reply({ content: "currently playing music on a channel :p", ephemeral: true });
+  }
+
+    if(interaction.options.getSubcommand == "play") {
+      await interaction.deferReply();
+
+      const query = interaction.options.getString("link");
+      const searchResult = await player
+          .search(query, {
+              requestedBy: interaction.user,
+              searchEngine: QueryType.AUTO
+          })
+          .catch(() => {});
+      if (!searchResult || !searchResult.tracks.length) return void interaction.followUp({ content: "you searched a non-existant song/music" });
+
+      const queue = await player.createQueue(interaction.guild, {
+          metadata: interaction.channel
       });
 
-      await interaction.reply("added music")
-    } else if (interaction.options.getSubcommand() === "skip") {
-      await interaction.reply("skipped current music!")
-    } else if (interaction.options.getSubcommand() === "stop") {
-      await interaction.reply("stopped the player!")
-    }
+      try {
+          if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+      } catch {
+          void player.deleteQueue(interaction.guildId);
+          return void interaction.followUp({ content: "could not join your voice channel (scary)" });
+      }
+
+      await interaction.followUp({ content: `loading your ${searchResult.playlist ? "playlist" : "track"}...` });
+      searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
+      if (!queue.playing) await queue.play();
+
+    } else if (interaction.options.getSubcommand() == "skip") {
+
+      await interaction.deferReply();
+      const queue = player.getQueue(interaction.guildId);
+      if (!queue || !queue.playing) return void interaction.followUp({ content: "no music is playing, you 0 iq" });
+      const currentTrack = queue.current;
+      const success = queue.skip();
+      return void interaction.followUp({
+        content: success ? `skipped \`${currentTrack}\`!` : "oh no, something went wrong"
+      });
+
+    } else if (interaction.options.getSubcommand() == "stop") {
+
+      await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+        if (!queue || !queue.playing) return void interaction.followUp({ content: "no music is playing, you 0 iq" });
+        queue.destroy();
+        return void interaction.followUp({ content: "stopped the player to blast music" });
+
+    };
   }
 }
